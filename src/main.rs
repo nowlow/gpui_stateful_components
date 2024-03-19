e415511fab3e379968181a76f3780d7c6e0e655e
Naoufel <nberrada@lithium.paris>
2024-03-19 15:36:49 +0100

    fix: cleaned code & tried new ways

--- src/main.rs
+++ src/main.rs
@@ -2,56 +2,72 @@ use gpui::{prelude::FluentBuilder, *};
 
 mod state;
 
-#[derive(IntoElement)]
-struct Sidebar {
-    bg_color: Option<Rgba>,
+use state::State;
+
+fn get_key(id: u8, key: &str) -> [u8; 16] {
+    let mut bytes = [0u8; 16];
+
+    bytes[0] = id;
+
+    let key_bytes = key.as_bytes();
+    for (i, &byte) in key_bytes.iter().enumerate().take(15) {
+        bytes[i + 1] = byte;
+    }
+    bytes
 }
 
-impl Sidebar {
-    fn build(cx: &mut WindowContext) -> Self {
+#[derive(IntoElement)]
+struct StatefulSquare {
+    id: u8,
+    bg_color: State<Rgba>,
+    count: State<u32>,
+}
+
+impl StatefulSquare {
+    fn build(id: u8, cx: &mut WindowContext) -> Self {
         Self {
-            bg_color: state::ComponentState::add(&"sidebar", rgb(0x0000ff), cx)
+            id,
+            bg_color: State::new(rgb(0x0000ff), get_key(id, "bg_color"), cx),
+            count: State::new(0, get_key(id, "count"), cx),
         }
     }
-
-    fn update(self, bg_color: Rgba, cx: &mut WindowContext) {
-        state::ComponentState::update::<Rgba>(&"sidebar", |this, _| {
-            *this = bg_color;
-        }, cx)
-    }
 }
 
-impl RenderOnce for Sidebar {
+impl RenderOnce for StatefulSquare {
     fn render(self, _cx: &mut WindowContext) -> impl IntoElement {
         div()
-            .id(1)
+            .id(self.id as i32)
+            .flex()
+            .justify_center()
             .w_10()
             .h_10()
-            // .bg(match self.bg_color {
-            //     Some(bg) => bg,
-            //     None => rgba(0xff0000)
-            // })
-            .when_some(self.bg_color, |this, bg_color| {
-                println!("some {:?}", bg_color);
-                this.bg(bg_color)
-            })
+            .justify_center()
+            .items_center()
+            .when_some(self.bg_color.get(), |this, bg_color| this.bg(bg_color))
+            .when_some(self.count.get(), |this, count| this.text_color(rgb( if count % 2 == 0 { 0xffffff } else { 0x000000 } )))
             .on_click(move |_this, cx| {
-                println!("Clicked");
-                state::ComponentState::update::<Rgba>(&"sidebar", |this, _| {
-                    println!("update {:?}", rgba(0x00ff00));
-                    *this = rgb(0x00ff00);
-                }, cx)
+                if let Some(count) = self.count.get() {
+                    if count % 2 == 0 {
+                        self.bg_color.update(rgb(0x00ff00), cx);
+                    } else {
+                        self.bg_color.update(rgb(0x0000ff), cx)
+                    }
+
+                    self.count.update(count + 1, cx);
+                }
+            })
+            .child(match self.count.get() {
+                Some(count) => format!("{}", count).to_owned(),
+                _ => "0".to_owned()
             })
     }
 }
 
-fn sidebar(cx: &mut WindowContext) -> impl IntoElement {
-    Sidebar::build(cx)
+fn stateful_square(id: u8, cx: &mut WindowContext) -> impl IntoElement {
+    StatefulSquare::build(id, cx)
 }
 
-struct MyApp {
-    is_pressed: bool,
-}
+struct MyApp {}
 
 impl Render for MyApp {
     fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
@@ -61,20 +77,11 @@ impl Render for MyApp {
             .justify_center()
             .items_center()
             .bg(rgb(0xffffff))
-            .child(sidebar(&mut cx.window_context()))
-            // .child(cx.new_view(|cx| {
-            //     div().w_10().h_10().bg(rgb(0x00ff00));
-            // }))
+            .gap_10()
+            .child(stateful_square(1, &mut cx.window_context()))
+            .child(stateful_square(2, &mut cx.window_context()))
     }
 }
-
-// new_sidebar::sidebar()
-//     .bg_color(self.sidebar_background_color)
-//     .on_click(cx.listener(move |this, b, view_cx| {
-//         // println!("click: {:?} {:?}", a, b);
-//         this.sidebar_background_color = 0x00ff00;
-//         this.is_pressed = !this.is_pressed;
-//     })),
 
 fn main() {
     App::new().run(|cx: &mut AppContext| {
@@ -90,9 +97,7 @@ fn main() {
             |cx| {
                 state::ComponentState::init(cx);
 
-                cx.new_view(|_cx| MyApp {
-                    is_pressed: false,
-                })
+                cx.new_view(|_cx| MyApp {})
             },
         );
     });
--- src/state.rs
+++ src/state.rs
@@ -1,10 +1,40 @@
 use gpui::*;
+// use uuid::Uuid;
+use hex;
 use std::{any::{ Any, TypeId }, collections::HashMap};
+
+#[derive(Clone, Copy)]
+pub struct State<T> {
+    value: Option<T>,
+    key: [u8; 16],
+}
+
+impl<T: 'static + Clone + Any> State<T> {
+    pub fn new(value: T, key: [u8; 16], cx: &mut WindowContext) -> Self {
+        let mut state = State {
+            value: None,
+            // key: Uuid::new_v4().into_bytes(),
+            key, // FIXME: use uuid instead, but it's unreliable since this will be rebuilt every time the component is rerendered
+        };
+
+        state.value = ComponentState::add(state.key, value, cx);
+
+        state
+    }
+
+    pub fn update(self, value: T, cx: &mut WindowContext) {
+        ComponentState::update(self.key, |this, _| *this = value, cx)
+    }
+
+    pub fn get(self) -> Option<T> {
+        self.value
+    }
+}
 
 #[derive(Clone)]
 pub struct ComponentState {
-    states: Model<HashMap<&'static str, Box<dyn Any>>>,
-    types: HashMap<&'static str, TypeId>,
+    states: Model<HashMap<String, Box<dyn Any>>>,
+    types: HashMap<String, TypeId>,
 }
 
 impl ComponentState {
@@ -19,14 +49,16 @@ impl ComponentState {
         this
     }
 
-    pub fn add<T: 'static + Any + Clone + core::fmt::Debug>(
-        key: &'static str,
+    pub fn add<T: 'static + Any + Clone>(
+        raw_key: [u8; 16],
         initial_value: T,
         cx: &mut WindowContext
     ) -> Option<T> {
         if !cx.has_global::<Self>() {
             return None;
         }
+
+        let key = hex::encode(raw_key);
 
         let type_id = TypeId::of::<T>();
 
@@ -36,13 +68,13 @@ impl ComponentState {
 
             println!("states types: {:?}", this.types);
 
-            if !this.types.contains_key(key) || this.types[key] != type_id {
+            if !this.types.contains_key(&key) || this.types[&key] != type_id {
                 println!("!existed {}", key);
-                this.states.update(cx, |this, cx| {
-                    this.insert(key, Box::new(initial_value.clone()));
+                this.states.update(cx, |this, _| {
+                    this.insert(key.clone(), Box::new(initial_value.clone()));
                 });
                 this.types.insert(key, type_id);
-            } else if let Some(boxed_any) = this.states.read(cx).get(key) {
+            } else if let Some(boxed_any) = this.states.read(cx).get(&key) {
                 println!("existed {}", key);
                 if let Some(value) = boxed_any.downcast_ref::<T>() {
                     existing = Some(value.clone());
@@ -50,27 +82,27 @@ impl ComponentState {
             }
         });
 
-        println!("current value: {:?}", existing.clone().or(Some(initial_value.clone())));
-
         existing.or(Some(initial_value))
     }
 
     pub fn update<T: 'static + Any>(
-        key: &'static str,
-        f: impl FnOnce(&mut T, &mut ModelContext<'_, HashMap<&str, Box<dyn Any>>>),
+        raw_key: [u8; 16],
+        f: impl FnOnce(&mut T, &mut ModelContext<'_, HashMap<String, Box<dyn Any>>>),
         cx: &mut WindowContext,
     ) {
         if !cx.has_global::<Self>() {
             return;
         }
 
+        let key = hex::encode(raw_key);
+
         let type_id = TypeId::of::<T>();
 
         cx.update_global::<Self, _>(|this, _cx| {
-            if let Some(&stored_type_id) = this.types.get(key) {
+            if let Some(&stored_type_id) = this.types.get(&key) {
                 if stored_type_id == type_id {
                     this.states.update(_cx, |this, _cx| {
-                        if let Some(any) = this.get_mut(key) {
+                        if let Some(any) = this.get_mut(&key) {
                             if let Some(value) = any.downcast_mut::<T>() {
                                 f(value, _cx);
                             }
